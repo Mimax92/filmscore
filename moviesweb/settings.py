9b8bb3213f82087f711c71149807b07b69c30c48
Maciej <mijasmaciej@gmal.com>
2020-09-29 21:10:39 +0200

    zmiana5

--- moviesweb/settings.py
+++ moviesweb/settings.py
@@ -23,7 +23,7 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["filmscore.herokuapp.com", "localhost:8000"]
+ALLOWED_HOSTS = ["filmscore.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
